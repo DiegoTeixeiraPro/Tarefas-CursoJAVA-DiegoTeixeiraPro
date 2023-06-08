e73ae0b6584b5e8205d554479bde4ea9b4be8257
DiegoTeixeiraPro <diegoateixeira.pro@gmail.com>
2023-06-08 16:39:31 -0300

    exercicio de fixacao

---
+++ 010_exercicios_comportamento_de_memoria_arrays_listas/exercicio1/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor digite quantos produtos serão " +
+                "cadastrados: ");
+        int n = scanner.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i < n; i++) {
+            scanner.nextLine();
+            System.out.print("Qual o produto? ");
+            String name = scanner.nextLine();
+            System.out.print("Qual o preço? ");
+            double price = scanner.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < vect.length; i++) {
+            soma += vect[i].getPrice();
+        }
+        double media = soma / vect.length;
+        System.out.printf("Preço médio dos produtos = %.2f%n", media);
+    }
+}
